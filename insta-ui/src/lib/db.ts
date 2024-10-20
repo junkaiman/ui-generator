2a4764244c48e7beae23789f28f20cfa0a8fafe4
Junkai Man <60953254+junkaiman@users.noreply.github.com>
2024-10-20 15:12:42 -0400

    add db mgmt for chats

---
+++ insta-ui/src/lib/db.ts
@@ -0,0 +1,51 @@
+import Dexie, { type EntityTable } from "dexie";
+import { Chat, Messages } from "./types";
+import { DBNames } from "./enums";
+import { v4 as uuidv4 } from "uuid";
+const db = new Dexie(DBNames.Chats) as Dexie & {
+  chats: EntityTable<Chat, "id">;
+};
+
+db.version(1).stores({
+  chats: "++id, messages, description, lastModified",
+});
+
+const addChat = async (messages: Messages) => {
+  const id = uuidv4();
+  const chat = {
+    id,
+    messages,
+    description: "Test chat",
+    lastModified: new Date(),
+  };
+  return db.chats.add(chat);
+};
+
+const updateChat = async (chat: Chat) => {
+  return db.chats.add(chat);
+};
+
+const getChatById = async (id: string) => {
+  return db.chats.get(id);
+};
+
+const deleteChatById = async (id: string) => {
+  return db.chats.delete(id);
+};
+
+const getChats = async () => {
+  return db.chats.toArray();
+};
+const clearChats = async () => {
+  return db.chats.clear();
+};
+
+export { db };
+export {
+  addChat,
+  updateChat,
+  getChatById,
+  deleteChatById,
+  getChats,
+  clearChats,
+};
--- insta-ui/src/lib/enums.ts
+++ insta-ui/src/lib/enums.ts
@@ -2,3 +2,10 @@ export enum PreviewerTabs {
   Preview = "preview",
   Code = "code",
 }
+
+/**
+ * DB related enums
+ */
+export enum DBNames {
+  Chats = "chats-database",
+}
