7d9601a94f0d243f0f8319ec434732e9c9150fe4
Junkai Man <60953254+junkaiman@users.noreply.github.com>
2024-10-28 13:31:08 -0400

    add side bar item

--- insta-ui/src/components/SideBar/index.tsx
+++ insta-ui/src/components/SideBar/index.tsx
@@ -1,7 +1,45 @@
+"use client";
+import { useRouter } from "next/navigation";
+import SideBarItem from "./SideBarItem";
+import { deleteChatById, getChats } from "@/lib/db";
+import { useEffect, useState } from "react";
+import { ChatHeader } from "@/lib/types";
+
 export default function SideBar() {
+  const router = useRouter();
+
+  const useChats = () => {
+    const [chats, setChats] = useState([] as ChatHeader[]);
+    useEffect(() => {
+      getChats().then((res: ChatHeader[]) => {
+        setChats(res);
+      });
+    }, [chats]);
+    return chats;
+  };
+
+  const chats = useChats();
+
+  const onChatSelect = (chatId: string) => {
+    router.push(`/c/${chatId}`);
+  };
+
+  const onChatDelete = (chatId: string) => {
+    console.log("Delete chat", chatId);
+    deleteChatById(chatId);
+  };
+
   return (
     <div>
-      SideBar
+      {chats.map((chat) => (
+        <SideBarItem
+          key={chat.id}
+          chatId={chat.id}
+          chatTitle={chat.description}
+          onChatSelect={onChatSelect}
+          onChatDelete={onChatDelete}
+        />
+      ))}
     </div>
   );
 }
