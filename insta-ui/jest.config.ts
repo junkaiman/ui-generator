dbe2a5e5a809f77a554768b8c0ba4c55b4cb886b
AlexM <96895622+ZhuoyangM@users.noreply.github.com>
2024-11-25 23:15:25 +0000

    fix jest configs

--- insta-ui/jest.config.ts
+++ insta-ui/jest.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
